8087b464280110e8c397b99d9e13b49ecda886d2
BuntyBru <buntybru@gmail.com>
2020-12-21 14:31:50 +0530

    config setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-const Pet = ({name,animal,breed}) =>{
-    return React.createElement('div',{},[
-        React.createElement('h1',{},name),
-        React.createElement('h2',{},animal),
-        React.createElement('h2',{},breed)
-    ])
-}
+import React from "react";
+import { render } from "react-dom";
+
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div", 
-        { id: 'something' }, 
-       [React.createElement("h1", {}, "Adopt Me!!"),
-        React.createElement(Pet, {name:'Luna',animal:'Dog',breed:'Havanese'}), 
-        React.createElement(Pet, {name:'Pepper',animal:'Dog',breed:'Cockateil'}), 
-        React.createElement(Pet, {name:'Doink',animal:'Cat',breed:'mixed'})])
-    
-
+  return React.createElement("div", { id: "something" }, [
+    React.createElement("h1", {}, "Adopt Me!!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Dog",
+      breed: "Cockateil",
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "mixed",
+    }),
+  ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+render(React.createElement(App), document.getElementById("root"));
